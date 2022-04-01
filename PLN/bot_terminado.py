57060e510bac8cfef9c330fee5e295eac21fa3d5
EmmanuelMMAstratos <78919881+EmmanuelMMAstratos@users.noreply.github.com>
2022-04-01 11:00:41 -0600

    bot
    terminado

--- PLN/bot_terminado.py
+++ PLN/bot_terminado.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Mar 31 18:49:37 2022
+Created on Fri Apr  1 08:51:23 2022
 
 @author: armma
 """
@@ -47,6 +47,17 @@ pares = [
             ["\nbot: Genial!, ¿En que puedo ayudarte?",]
             
         ],
+        
+        [
+            r"¿(.*) ip a binario?",
+            ["\nbot: Np es complicado pasar una ip a binario, primeramrente tenemos que una ip son 32 bits, y en redes ipv4 se dividen en 4 partes, entonces tendríamos 4 octetos, se verían así, 11111111.11111111.11111111.11111111, ahí son 32 bits y separados en 4 octetos por un punto, ahora tomando un octeto ahora tendríamos que cada uno tiene un valor independiente, del cual SIEMPRE SE LEE DE DERECHA A IZQUIERDA, empezando por derecha tenemos 1=1, el siguiente sería 1=2, luego 1=4, luego = 1=8,luego 1=16,luego 1=32,luego 1=64,luego 1=128, ya viendolo mejor tendríamos el siguiente, 11111111=> 1=128 1=64 1=32 1=16 1=8 1=4 1=2 1=1, una vez que ya tengas esto solo es cosa de ver cuales podemos encender para posterior sumarlos como normalmente se suma, hasta encontrar el valor indicado que hace alucion al número que tenemos, Un EJEMPLO sería el siguiente: tenemos 175.32.4.6 , bueno tomamos el primer octecto que hay, que es 175, ahora sebemos encender ciertos binarios de ese octeto para que nos dé '175', recordemos que tenemos los valores de cada binario que son 1=128 1=64 1=32 1=16 1=8 1=4 1=2 1=1,ahora haciendo una primera prueba, quiero ocupar estos bianrios, y se verían así,  11000000 => 1=128 1=64 1=0 1=0 1=0 1=0 1=0 1=0, así se vería, como estan apagados los demás, se les da un valor a cero ahora sumandolo tendríamos el siguiente valor 128+64+0+0+0+0+0+0=192, no tenemos el número esperado, entonces tenemos que apagar uno porque ya nos pasamos y entoces tendremos que encender otro encedieramos uno más, 10100000 => 1=128 1=0 1=32 1=0 1=0 1=0 1=0 1=0, entonce si lo sumamos quedaría así , 128+0+32+0+0+0+0+0=160, ya tenemos un número menor, ahora nos hace falta 15 para llegar a 175, así que tenemos que encender unos más para llegar a 175,entonces encendemos unos más 10101000=>1=128 1=0 1=32 1=0 1=8 1=0 1=0 1=0, y teniendo la suma, tendríamos algo así, 128+0+32+0+8+0+0+0 = 168, entonces volvemos a activar unos bianrios más (los que consideres),10101110 => 1=128 1=0 1=32 1=0 1=8 1=4 1=2 1=0, ahora tendríamos una suma como esta, 128+0+32+0+8+4+2+0 = 174, bueno, ahora solo nos hace falta un 1 para llegar a 175, entonce encenderíamo uno último, 10101111 => 1=128 1=0 1=32 1=0 1=8 1=4 1=2 1=1, ahora tendríamos una suma como esta, 128+0+32+0+8+4+2+1 = 171, así tendríamos el primer número en binario del primer octecto, solo restaría hacer los sigiuentes 3, es algo sencillo, mientras más lo hagas, será mas facil para ti calcularlo",]
+            
+        ],
+        [
+            r"¿(.*) ipv4 a binario?",
+            ["\nbot: Genial!, ¿En que puedo ayudarte?",]
+            
+        ],
         [
             r"¿(.*)es una red ipv4?",
             ["\nbot : 'Este es el Protocolo de Internet versión 4 (en inglés, Internet Protocol version 4, IPv4) es la cuarta versión del Internet Protocol (IP), un protocolo de interconexión de redes basados en Internet, y que fue la primera versión implementada en 1983 para la producción de ARPANET. Definida en el RFC 791, el IPv4 usa direcciones de 32 bits, limitadas a {\displaystyle 2^{32}}2^{{32}} = 4 294 967 296 direcciones únicas, muchas de ellas LAN.1​ Por el crecimiento enorme que ha tenido la seguridad electrónica y la automatización, combinado con el hecho de que hay desperdicio de direcciones en muchos casos (consultar las secciones que siguen), ya hace varios años se observó que escaseaban las direcciones IPv4.' ",]
@@ -64,7 +75,7 @@ pares = [
         ],
         [
             r"¿(.*) calcular (.*) ipv4?",
-            ["\nbot : Relamente calcular un red ipv4, no existe tal acción, lo que se puede saber es como calcular la mascara de subred, la wildcard, una dirección de subred, como pasar una ip a binario y pasar una ip binaria a notación punteada",]
+            ["\nbot : Relamente calcular un red ipv4, no existe tal acción, lo que se puede saber es como calcular la mascara de subred, la wildcard, una dirección de red, como pasar una ip a binario y pasar una ip binaria a notación punteada",]
             
         ],
         [
@@ -74,6 +85,16 @@ pares = [
         ],
         [
             r"¿(.*) utilizan (.*) ipv4?",
+            ["\nbot : Se ocuapn en todos lados, ahora es el protocolo ipv4 con el ipv6, en tu telefono cuando ves las configuraciones avanzadas de la red, se te asigna una ipv4 y una ipv6 para poder conectarte",]
+            
+        ],
+        [
+            r"¿(.*) utilizan (.*) redes",
+            ["\nbot : Se ocuapn en todos lados, ahora es el protocolo ipv4 con el ipv6, en tu telefono cuando ves las configuraciones avanzadas de la red, se te asigna una ipv4 y una ipv6 para poder conectarte",]
+            
+        ],
+        [
+            r"¿(.*) aplicar (.*) ipv4?",
             ["\nbot : Se ocuapn en todos lados, ahora es el protocolo ipv4 con el ipv6, en tu telefono cuando ves las configuraciones avanzadas de la red, se te asigna una ipv4 y una ipv6 para poder conectarte",]
             
         ],
@@ -133,6 +154,16 @@ pares = [
             
         ],
         [
+            r"¿(.*) calcular (.*) máscara de subred?",
+            ["\nbot : Para calcular una mascara de sub red, es algo sencillo, sabiendo la clase y los bits que pertenecen a red, solo sería cosa de convertir los binarios a notacion decimal punteada, ejemplo : Tenemos 192.168.24.2/24, ahora lo pasamos a binario el número de bits de red, 11111111.11111111.11111111.00000000 y quedaría de esta manera, ya solo sería pasarlo a notacion decimal punteada, y quedaría así, 255.255.255.0",]
+            
+        ],
+        [
+            r"¿(.*) calcular (.*) submascara de red?",
+            ["\nbot : Para calcular una mascara de sub red, es algo sencillo, sabiendo la clase y los bits que pertenecen a red, solo sería cosa de convertir los binarios a notacion decimal punteada, ejemplo : Tenemos 192.168.24.2/24, ahora lo pasamos a binario el número de bits de red, 11111111.11111111.11111111.00000000 y quedaría de esta manera, ya solo sería pasarlo a notacion decimal punteada, y quedaría así, 255.255.255.0",]
+            
+        ],
+        [
             r"¿(.*) son (.*) subredes?",
             ["\nbot : Las subredes son un método para maximizar el espacio de direcciones IPv4 de 32 bits y reducir el tamaño de las tablas de enrutamiento en una interred mayor. En cualquier clase de dirección, las subredes proporcionan un medio de asignar parte del espacio de la dirección host a las direcciones de red, lo cual permite tener más redes.",]
             
@@ -182,6 +213,39 @@ pares = [
             ["\nbot : Las subredes son un método para maximizar el espacio de direcciones IPv4 de 32 bits y reducir el tamaño de las tablas de enrutamiento en una interred mayor. En cualquier clase de dirección, las subredes proporcionan un medio de asignar parte del espacio de la dirección host a las direcciones de red, lo cual permite tener más redes. La parte del espacio de dirección de host asignada a las nuevas direcciones de red se conoce como número de subred.",]
             
         ],
+        
+        [
+            r"¿(.*) es (.*) máscara de red?",
+            ["\nbot :  la máscara de subred tiene la misma apariencia que una dirección IP, pero en este caso, va a ser la que nos ayude a identificar si un host está dentro de una subred local o en una red remota.",]
+            
+        ],
+        [
+            r"¿(.*) máscara de (.*) red?",
+            ["\nbot :  la máscara de subred tiene la misma apariencia que una dirección IP, pero en este caso, va a ser la que nos ayude a identificar si un host está dentro de una subred local o en una red remota.",]
+            
+        ],
+        [
+            r"¿(.*) máscara de red?",
+            ["\nbot :  la máscara de subred tiene la misma apariencia que una dirección IP, pero en este caso, va a ser la que nos ayude a identificar si un host está dentro de una subred local o en una red remota.",]
+            
+        ],
+        [
+            r"¿(.*) es (.*) máscaras de red?",
+            ["\nbot : Las subredes son un método para maximizar el espacio de direcciones IPv4 de 32 bits y reducir el tamaño de las tablas de enrutamiento en una interred mayor. En cualquier clase de dirección, las subredes proporcionan un medio de asignar parte del espacio de la dirección host a las direcciones de red, lo cual permite tener más redes. La parte del espacio de dirección de host asignada a las nuevas direcciones de red se conoce como número de subred.",]
+            
+        ],
+        [
+            r"¿(.*) máscaras de (.*) red?",
+            ["\nbot : Las subredes son un método para maximizar el espacio de direcciones IPv4 de 32 bits y reducir el tamaño de las tablas de enrutamiento en una interred mayor. En cualquier clase de dirección, las subredes proporcionan un medio de asignar parte del espacio de la dirección host a las direcciones de red, lo cual permite tener más redes. La parte del espacio de dirección de host asignada a las nuevas direcciones de red se conoce como número de subred.",]
+            
+        ],
+        [
+            r"¿(.*) máscaras de red?",
+            ["\nbot : Las subredes son un método para maximizar el espacio de direcciones IPv4 de 32 bits y reducir el tamaño de las tablas de enrutamiento en una interred mayor. En cualquier clase de dirección, las subredes proporcionan un medio de asignar parte del espacio de la dirección host a las direcciones de red, lo cual permite tener más redes. La parte del espacio de dirección de host asignada a las nuevas direcciones de red se conoce como número de subred.",]
+            
+        ],
+        
+        
         [
             r"¿(.*) es (.*) notacion decimal punteada?",
             ["\nbot : La notación decimal con puntos es un sistema de presentación de números que es un poco diferente de las convenciones comunes en aritmética como se enseña en las escuelas. Específicamente, la notación decimal con puntos se usa en varios contextos de TI, incluidas las direcciones de protocolo de Internet.",]
@@ -247,11 +311,7 @@ pares = [
             ["\nbot : Este viene al final de la red, que no es mas que un numero para saber cuantos bits pertenecen al de red, por ejemplo 192.16.3.5 /24, el cual no esta diciendo que tenemos 24 bits de red, y son 192.16.3",]
             
         ],
-        [
-            r"¿(.*) calcular (.*) mascara de subred?",
-            ["\nbot : Para calcular una mascara de sub red, es algo sencillo, sabiendo la clase y los bits que pertenecen a red, solo sería cosa de convertir los binarios a notacion decimal punteada, ejemplo : Tenemos 192.168.24.2/24, ahora lo pasamos a binario el número de bits de red, 11111111.11111111.11111111.00000000 y quedaría de esta manera, ya solo sería pasarlo a notacion decimal punteada, y quedaría así, 255.255.255.0",]
-            
-        ],
+        
         [
             r"¿(.*) calcular (.*) direccion de red?",
             ["\nbot : sabiendo que el tenemos la ip, concemos su clase, y su mascara de subred, solo es cosa de tener los binarios de la ip y de la mascara de subred, con ello tendríamos que, hacer una operación AND, por ejemplo:192.168.55.44 una mascara de subred 255.255.255.0, ahora pasandolo a binarios, tendríamos que 192.168.55.44 => 1100000000.10101000.00110111.00101100  y de mascara de subred 255.255.255.0 => 11111111.11111111.11111111.00000000, ahora si aplicamos un AND  a estos dos, tendríamos algo así: 1100000000.10101000.00110111.00101100 AND 11111111.11111111.11111111.00000000 = 11000000.10101000.00110111.00000000, y si lo pasamos a notacion punteada, tendríamos algo así: 11000000.10101000.00110111.00000000 = 192.168.55.0, y esta sería su dirección de red",]
@@ -301,6 +361,36 @@ pares = [
         [
             r"¿(.*) es (.*) enrutamiento?",
             ["\nbot : Este es el proceso de reenviar paquetes entre redes, buscando la ruta más corta",]
+            
+        ],
+        [
+            r"¿(.*) es (.*) wildcard?",
+            ["\nbot : La wildcard es la máscara inversa de la submascara de red, se utilizan más en las listas de control de acceso, un ejemplo sencillo sería que tenemos una máscara de red de 255.255.255.0, entonces la wildcard sería 0.0.0.255, pasando a un ejemplo mas complejo, teneos una submascara de red de 255.255.255.252 y entonces lo inverso es 0.0.0.3, de una manera más coloquial y facil de comprender es que son los bits que le hacen falta a un octecto de la mascara de red para llegar a 255, ejemplo tenemos una submascara de red 255.252.0.0 y lo inverso a ello , sería 0.3.255.255",]
+            
+        ],
+        [
+            r"¿(.*) wildcard?",
+            ["\nbot : La wildcard es la máscara inversa de la submascara de red, se utilizan más en las listas de control de acceso, un ejemplo sencillo sería que tenemos una máscara de red de 255.255.255.0, entonces la wildcard sería 0.0.0.255, pasando a un ejemplo mas complejo, teneos una submascara de red de 255.255.255.252 y entonces lo inverso es 0.0.0.3, de una manera más coloquial y facil de comprender es que son los bits que le hacen falta a un octecto de la mascara de red para llegar a 255, ejemplo tenemos una submascara de red 255.252.0.0 y lo inverso a ello , sería 0.3.255.255",]
+            
+        ],
+        [
+            r"¿(.*) calcular (.*) wildcard?",
+            ["\nbot : No es complicado, solo es lo inverso a la mascara de red, por ejemplo, tenemos una máscara de red de 255.255.255.0, entonces la wildcard sería 0.0.0.255, pasando a un ejemplo mas complejo, teneos una submascara de red de 255.255.255.252 y entonces lo inverso es 0.0.0.3, de una manera más coloquial y facil de comprender es que son los bits que le hacen falta a un octecto de la mascara de red para llegar a 255, ejemplo tenemos una submascara de red 255.252.0.0 y lo inverso a ello , sería 0.3.255.255",]
+            
+        ],
+        [
+            r"¿(.*) es (.*) broadcast?",
+            ["\nbot : El broadcast es un mensaje que se transmite a todos los miembros de una red y que no necesita ninguna acción de retroalimentación.",]
+            
+        ],
+        [
+            r"¿(.*) broadcast?",
+            ["\nbot : El broadcast es un mensaje que se transmite a todos los miembros de una red y que no necesita ninguna acción de retroalimentación.",]
+            
+        ],
+        [
+            r"¿(.*) calcular (.*) broadcast?",
+            ["\nbot : Para calcular el broad cast, no es algo complicado, una ves teniendo la dirección de red, y la wildcard, sería una unión de estas, de manera coloquial, si tenemos un direccionamieto como este: 192.235.6.0, y su wildcar es 0.0.0.255, entonces tendríamos una operación AND (11000000.11101011.00000110.00000000 AND 00000000.00000000.00000000.11111111 => 11000000.11101011.00000110.11111111 = 192.235.6.255)",]
             
         ],
         
